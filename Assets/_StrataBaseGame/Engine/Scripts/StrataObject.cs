6fa9d3a1add4373539edeef0d446f1a499f0f72c
TechSide <68258126+techsideofficial@users.noreply.github.com>
2024-05-19 10:41:46 +1200

    Init

---
+++ Assets/_StrataBaseGame/Engine/Scripts/StrataObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrataObject : MonoBehaviour
+{
+    public string ObjectName;
+    public bool ChunkedObject;
+    public bool HasPhysics;
+    public bool CoreObject;
+    public bool Static;
+    public bool Advanced;
+    public bool ObjectSet;
+    public string ObjectCreator;
+    public string Description;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # StrataRRS
  RRS version of Strata Engine, now open source!
+
+ ## Folder Layout
+
+ - Assets/_StrataBaseGame/Content (This is where your game content goes. Anything that doesn't fit in those categories goes in the common folder.)
